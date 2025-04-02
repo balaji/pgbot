14ceeb5808f22ddd42b20672491f1c914787ce57
Balaji Damodaran <damodaran.balaji@gmail.com>
2025-04-03 00:20:21 +0200

    first commit

---
+++ pgbot-webapp/src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@/components/ui/button'
+import '@/styles/globals.css'
+ 
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Hello</Button>
+    </div>
+  )
+}
