f04e98817448d4a60ee9bddc6b136af551db6e0c
Balaji Damodaran <damodaran.balaji@gmail.com>
2025-04-03 19:23:12 +0200

    simpler in structure

--- start.sh
+++ start.sh
@@ -10,9 +10,7 @@ echo "Installing requirements..."
 pip install -r ./requirements.txt
 
 echo "Starting Supabase..."
-cd ./db || exit 1
 docker compose up -d
 
 echo "Load articles..."
-cd ../ || exit 1
 python scripts/load_data.py
