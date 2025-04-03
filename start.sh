d94bbe7b4a10f948316863fd8a3d0f5868ad88dd
Balaji Damodaran <damodaran.balaji@gmail.com>
2025-04-03 19:44:25 +0200

    tiny workaround for one-click setup

--- start.sh
+++ start.sh
@@ -9,8 +9,16 @@ source ./pgbot_venv/bin/activate
 echo "Installing requirements..."
 pip install -r ./requirements.txt
 
+running=$(docker compose ps | grep -i healthy | wc -l)
+
 echo "Starting Supabase..."
 docker compose up -d
+
+if [[ $running == "0" ]]; then
+# first time docker compose run doesn't get the database ready. I don't know why this is a workaround
+  echo "This is a cold start, refreshing docker containers as a wrorkaround..."
+  docker compose up -d
+fi
 
 echo "Load articles..."
 python scripts/load_data.py
