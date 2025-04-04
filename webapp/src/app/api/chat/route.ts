9e9fc84c6771f5fe6d9bf6000c68d7d85ab78d33
Balaji Damodaran <damodaran.balaji@gmail.com>
2025-04-04 15:34:53 +0200

    minor fixes

--- webapp/src/app/api/chat/route.ts
+++ webapp/src/app/api/chat/route.ts
@@ -10,11 +10,9 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 
-process.env.SUPABASE_PRIVATE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
-process.env.SUPABASE_URL = "http://localhost:8000";
 const supabaseClient = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_PRIVATE_KEY,
+    process.env.SUPABASE_PUBLIC_URL!,
+    process.env.ANON_KEY!,
 );
 
 const embeddings = new OpenAIEmbeddings({
